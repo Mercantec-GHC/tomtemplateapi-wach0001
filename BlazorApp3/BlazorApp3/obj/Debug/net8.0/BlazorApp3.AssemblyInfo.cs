e21830c7330e6d2b0ad1dbc80c6f3b8ebfd6c2c7
wach0001 <146825284+wach0001@users.noreply.github.com>
2024-08-09 09:46:00 +0200

    ud

--- BlazorApp3/BlazorApp3/obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
+++ BlazorApp3/BlazorApp3/obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57df2eb84e6129ff033d7dfa3397e303ec69cce5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e20f5449d54c89c5bb699df121829003669882b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
